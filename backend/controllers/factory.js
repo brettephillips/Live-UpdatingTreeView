eb972351aefad54ce96735a114248ba73cf3e74e
brettephillips <bep4144@rit.edu>
2019-01-05 13:18:26 -0500

    Factory API Endpoints
    Completion of the factory api endpoints

--- backend/controllers/factory.js
+++ backend/controllers/factory.js
@@ -1,7 +1,54 @@
-exports.index = function(req, res) {
-    res.json({
-        status: "success",
-        message: "Contacts retrieved successfully",
-        data: "contacts"
-    });
+//Import needed packages
+let models = require('../models/index');
+
+/**
+ * Function that will get all of the factories
+ * from the DB.
+ */
+
+exports.retrieve = (req, res) => {
+    models.Factory.findAll().then(factories => res.json(factories));
+};
+
+/**
+ * Function that will create a factory given
+ * input from the user.
+ */
+
+exports.create = (req, res) => {
+    models.Factory.create( { 
+        name: req.body.name, 
+        lower_bound: req.body.lower_bound, 
+        upper_bound: req.body.upper_bound
+    }).then(factory => res.json(factory));
+};
+
+/**
+ * Function that will update a specific factory
+ * based on the factory's id.
+ */
+
+exports.update = (req, res) => {
+    models.Factory.update( {
+        name: req.body.name, 
+        lower_bound: req.body.lower_bound, 
+        upper_bound: req.body.upper_bound
+    }, {
+        where: {
+            id: req.params.id
+        }
+    }).then(factory => res.json(factory));
+};
+
+/**
+ * Function that will delete a factory from the DB
+ * based on the factory's id.
+ */
+
+exports.remove = (req, res) => {
+    models.Factory.destroy( {
+        where: {
+            id: req.params.id
+        }
+    }).then(factory => res.json(factory));
 };
--- backend/routes/factory.js
+++ backend/routes/factory.js
@@ -5,7 +5,12 @@ let factoryController = require('../controllers/factory');
 
 //Set the routes for the factories
 router.route('/factories')
-    .get(factoryController.index);
+    .get(factoryController.retrieve)
+    .post(factoryController.create);
+
+router.route('/factories/:id')
+    .put(factoryController.update)
+    .delete(factoryController.remove);
 
 //Export the routes
 module.exports = router;
